702355365275b53acac02482126a4e91112d2586
MrPancakes39 <salmannhassan39@gmail.com>
2022-09-01 14:13:30 +0300

    Created a simple Hello Express Server

--- index.mjs
+++ index.mjs
@@ -1,3 +1,9 @@
 #!/bin/env node
 
-console.log("Hello World!");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
+app.use(express.static("public"));
