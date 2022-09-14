91a16dd132773eab63e78fe4a5c55bd425085719
MrPancakes39 <salmannhassan39@gmail.com>
2022-09-14 20:19:45 +0300

    Merged dom.js back into main.js

--- public/main.js
+++ public/main.js
@@ -1,3 +1,37 @@
+window.$ = {
+    one: (...args) => window.top.document.querySelector(...args),
+    all: (...args) => window.top.document.querySelectorAll(...args),
+    on: (dom, eventName, func) => dom.addEventListener(eventName, func),
+    off: (dom, eventName, func) => dom.removeEventListener(eventName, func),
+    make: (domString) => {
+        const wrapper = document.createElement("div");
+        wrapper.innerHTML = domString;
+        return wrapper.firstElementChild;
+    },
+    attr: (dom, attr, value) => {
+        attr = attr.toString();
+        if (typeof value == "undefined") {
+            return dom.getAttribute(attr);
+        }
+        dom.setAttribute(attr, value.toString());
+    },
+    find: (dom, ...args) => dom.querySelector(...args),
+    css: (dom, attr, value) => {
+        attr = attr.toString();
+        if (typeof value == "undefined") {
+            return window.getComputedStyle(dom)[attr];
+        }
+        dom.style[attr] = value.toString();
+    },
+    prop: (dom, prop) => window.getComputedStyle(dom).getPropertyValue(prop),
+    pos: (dom) => {
+        return {
+            top: dom.offsetTop,
+            left: dom.offsetLeft,
+        };
+    },
+};
+
 window.onload = function () {
     for (let i = 1; i <= 20; i++) {
         addToList(`File ${i}`);
