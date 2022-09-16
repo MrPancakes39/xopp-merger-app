83a569efd65ac9bad1f313499d923a46f80f8bcd
MrPancakes39 <salmannhassan39@gmail.com>
2022-09-16 11:08:01 +0300

    Added basic POST route

--- index.mjs
+++ index.mjs
@@ -3,7 +3,18 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
 app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
 app.use(express.static("public"));
+
+app.use(express.json());
+
+app.post("/api/merge", (req, res) => {
+    mergeFiles(req.body);
+    res.send("OK");
+});
+
+function mergeFiles(data) {
+    console.log(data);
+}
