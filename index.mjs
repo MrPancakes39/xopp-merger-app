0199569d3b14202dca0515f6ee029ed28393779a
MrPancakes39 <salmannhassan39@gmail.com>
2022-09-16 14:17:22 +0300

    Added CORS

--- index.mjs
+++ index.mjs
@@ -1,14 +1,16 @@
 #!/bin/env node
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
+app.use(cors());
+app.use(express.json());
 app.use(express.static("public"));
 
-app.use(express.json());
+app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
 
 app.post("/api/merge", (req, res) => {
     const file = mergeFiles(req.body);
